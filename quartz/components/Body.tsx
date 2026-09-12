0342cd36e17e01e13c4964125ffc4807945a1861
Joshua Peter <joshuajpeter@gmail.com>
2026-09-12 13:18:14 -0500

    Quartz sync: Sep 12, 2026, 1:18 PM

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -1,7 +1,47 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  return <div id="quartz-body">{children}</div>
+  return (
+    <>
+      <div id="dappled-light" aria-hidden="true">
+        <div id="glow" />
+        <div id="glow-bounce" />
+        <div class="perspective">
+          <div id="leaves" />
+          <div id="blinds">
+            <div class="shutters">
+              <div class="shutter" />
+              <div class="shutter" />
+              <div class="shutter" />
+              <div class="shutter" />
+              <div class="shutter" />
+            </div>
+            <div class="vertical">
+              <div class="bar" />
+              <div class="bar" />
+              <div class="bar" />
+              <div class="bar" />
+              <div class="bar" />
+              <div class="bar" />
+            </div>
+          </div>
+        </div>
+        <div id="progressive-blur">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+        <svg aria-hidden="true" width="0" height="0">
+          <filter id="wind">
+            <feTurbulence type="fractalNoise" baseFrequency="0.01 0.03" numOctaves="2" />
+            <feDisplacementMap in="SourceGraphic" scale="30" />
+          </filter>
+        </svg>
+      </div>
+      <div id="quartz-body">{children}</div>
+    </>
+  )
 }
 
 export default (() => Body) satisfies QuartzComponentConstructor
